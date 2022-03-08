4783277154680dcf27a973a80c1079e40df38325
Jeff Eldredge <jdeldre@g.ucla.edu>
2022-03-08 11:49:03 -0800

    add tests and notebooks

---
+++ test/literate/0.0-Index.jl
@@ -0,0 +1,21 @@
+include("header.jl")
+
+#=
+These Jupyter notebooks and associated code serve as a companion to the course.
+
+These are not static notebooks. **You are encouraged to play with them: run with different parameter values, create other plots, etc.!** The whole point is to enable an understanding of fluid mechanics through interactiveness.
+
+*The code is released under the [MIT license](https://opensource.org/licenses/MIT).*
+<hr />
+=#
+
+#=
+## Table of Notebooks
+
+- [Introduction](0.1-WelcomeToJulia.ipynb)
+
+#### Chapter 1 -
+
+-
+
+=#
